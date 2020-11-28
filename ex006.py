957ba421d36f1a57665f5e8010e3fa7087c6087a
Anderson Felix <andersonfelixmsouza@gmail.com>
2020-11-28 12:54:51 -0300

    Create ex006.py

---
+++ ex006.py
@@ -0,0 +1,5 @@
+num = int(input('Digite um número : '))
+
+print('Dobro : {} | Triplo : {} | Raiz quadrada : {:.2f}'.format(num*2,num*3,pow(num, (1/2))))
+
+# O método pow() calcula a raiz quadrada, no caso com a base = 'num' e o expoente = '(1/2)' :meio:
